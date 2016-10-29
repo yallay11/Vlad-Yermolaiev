bf7e967f2dcb03acc2c5f9ccad25de21228db8fb
yallay11 <vladman98.ua@gmail.com>
2016-10-29 17:10:51 +0300

    Add files via upload

---
+++ vinzenere.c
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+#include <cs50.h> 
+#include <string.h> 
+#include <ctype.h> 
+
+
+int main(int argc, string argv[])
+{
+if (argc != 2) {
+printf("Try again\n");
+return 1;
+}
+string key  = argv[1];
+for (int k = 0, n = strlen(key); k < n; k++) {
+if (!isalpha(key[k])) {
+printf("Try again\n");
+return 1;
+}
+}
+string txt = GetString();
+if (txt == NULL){ 
+printf("Try again");
+return 1;
+}
+for (int i = 0,j=0; i < strlen(txt); i++)
+{
+if (isalpha(txt[i]))
+{
+
+
+if (islower(txt[i]))
+printf("%c", ((((txt[i] - 'a') + ((tolower(key[j++%strlen(key)])) - 'a') % 26) % 26) + 'a'));
+
+
+if (isupper(txt[i]))
+printf("%c", ((((txt[i] - 'A') + ((toupper(key[j++%strlen(key)])) - 'A') % 26) % 26) + 'A'));
+}
+else
+printf("%c", txt[i]);
+}
+printf("\n");
+return 0;
+}
